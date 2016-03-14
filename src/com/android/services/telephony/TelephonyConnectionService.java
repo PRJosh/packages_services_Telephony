d8109e10d24ccc898e91f84e0cf0cf54efe295b6
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2016-03-14 03:56:06 -0800

    Fix "Fix "multi-sim: fallback on active subId on emergency calls""
    There's no phoneId without extphone. Go back to the original implementation
    of resorting to the defaultphone.
    
    Change-Id: I2b300d27f84be3489005f9630ed5cc0ec7c34599

--- src/com/android/services/telephony/TelephonyConnectionService.java
+++ src/com/android/services/telephony/TelephonyConnectionService.java
@@ -639,7 +639,7 @@ public class TelephonyConnectionService extends ConnectionService {
             } catch (NullPointerException ex) {
                 Log.e(this, ex, "Exception : " + ex);
             }
-            return PhoneFactory.getPhone(phoneId);
+            return PhoneFactory.getDefaultPhone();
         }
 
         int subId = PhoneUtils.getSubIdForPhoneAccountHandle(accountHandle);
